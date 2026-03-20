65d76b91a2eab4c35e591bef8d67ae1f908ecfea
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-03-20 09:35:46 +0100

    Update engine-requirements.js

--- engine-requirements.js
+++ engine-requirements.js
@@ -4,7 +4,7 @@ if (major < 20) {
   console.error(
     `\n❌ This package requires Node.js 20+ to run reliably.\n` +
     `   You are using Node.js ${process.versions.node}.\n` +
-    `   Please upgrade to Node.js 20+ to proceed.\n`
+    `   Please upgrade to Node.js 20+ to proceed..\n`
   );
   process.exit(1);
 }
